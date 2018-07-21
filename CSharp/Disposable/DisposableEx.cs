2ea7c64473e0f6c4fd96334c0c6894d59c22e862
Boris <b.shuvyrdenkov@gmail.com>
2018-07-21 22:30:27 +0300

    Add disposable kata.

---
+++ CSharp/Disposable/DisposableEx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CodeKatas.CSharp.Disposable
+{
+	/// <summary>
+	/// Example of disposing pattern.
+	/// </summary>
+	public class DisposableEx : IDisposable
+	{
+		private bool _alreadyDisposed;
+
+		// Destructor when needed
+		//~DisposableEx()
+		//{
+		//	Dispose(false);
+		//}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (_alreadyDisposed)
+				return;
+
+			if (disposing)
+			{
+				// free managed resources
+			}
+
+			// free unmanaged resources
+
+			_alreadyDisposed = true;
+		}
+
+		public void DoSomething()
+		{
+			if (_alreadyDisposed)
+				throw new ObjectDisposedException("Object already disposed!");
+		}
+	}
+}

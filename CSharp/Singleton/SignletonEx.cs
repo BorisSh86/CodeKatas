2f22b2cb36c0d5f9283f9f463c4051733921d2db
Boris <b.shuvyrdenkov@gmail.com>
2018-07-24 23:25:29 +0300

    Add singleton examples.

---
+++ CSharp/Singleton/SignletonEx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace CodeKatas.CSharp.Singleton
+{
+	/// <summary>
+	/// Singleton with lazy implementation.
+	/// </summary>
+	public class SignletonLazy
+	{
+		private static readonly Lazy<SignletonLazy> _instance = new Lazy<SignletonLazy>(() => new SignletonLazy());
+
+		public static SignletonLazy Instance => _instance.Value;
+
+		private SignletonLazy()
+		{
+		}
+	}
+
+	/// <summary>
+	/// Singleton with double check locking implementation.
+	/// </summary>
+	public class SingletonDoubleCheckLocking
+	{
+		private static volatile SingletonDoubleCheckLocking _instance;
+
+		private static readonly object _syncLock = new object();
+
+		public static SingletonDoubleCheckLocking Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					lock (_syncLock)
+					{
+						if (_instance == null)
+						{
+							_instance = new SingletonDoubleCheckLocking();
+						}
+					}
+				}
+
+				return _instance;
+			}
+		}
+
+		private SingletonDoubleCheckLocking()
+		{
+		}
+	}
+
+	/// <summary>
+	/// Singleton with static implementations.
+	/// </summary>
+	public class SingletonStatic
+	{
+		private static readonly SingletonStatic _instance = new SingletonStatic();
+
+		public static SingletonStatic Instance => _instance;
+
+		static SingletonStatic()
+		{
+		}
+
+		private SingletonStatic()
+		{
+		}
+	}
+}

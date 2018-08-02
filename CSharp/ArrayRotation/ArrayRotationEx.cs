8a98b8046c06b3ab3b220ee732bfd4af69b06125
Boris <b.shuvyrdenkov@gmail.com>
2018-08-02 19:22:06 +0300

    Add array rotation example.

---
+++ CSharp/ArrayRotation/ArrayRotationEx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.CSharp.ArrayRotation
+{
+	/// <summary>
+	/// Array rotation implementation.
+	/// </summary>
+	public static class ArrayRotationEx
+	{
+		/// <summary>
+		/// Rotate array with for loop.
+		/// </summary>
+		/// <param name="source">Source array.</param>
+		/// <param name="shift">Rotation count.</param>
+		/// <returns>Rotated array.</returns>
+		public static int[] RotateArrayWithFor(int[] source, int shift)
+		{
+			if (source == null || source.Length == 0)
+				return source;
+
+			shift %= source.Length;
+			if (shift == 0)
+				return source;
+
+			int[] rotated = new int[source.Length];
+
+			for (int i = 0; i < source.Length; i++)
+			{
+				int rotatedIndex;
+
+				if ((i + shift) < source.Length && (i + shift) >= 0)
+				{
+					rotatedIndex = i + shift;
+				}
+				else
+				{
+					rotatedIndex = Math.Abs(source.Length - Math.Abs(i + shift));
+				}
+
+				rotated[rotatedIndex] = source[i];
+			}
+
+			return rotated;
+		}
+
+		/// <summary>
+		/// Rotate array with queue.
+		/// </summary>
+		/// <param name="source">Source array.</param>
+		/// <param name="shift">Rotation count.</param>
+		/// <returns>Rotated array.</returns>
+		public static int[] RotateArrayWithQueue(int[] source, int shift)
+		{
+			if (source == null || source.Length == 0)
+				return source;
+
+			shift %= source.Length;
+			if (shift == 0)
+				return source;	
+			
+			Queue<int> queue = new Queue<int>(shift > 0 ? source.Reverse() : source);
+
+			for (int i = 0; i < Math.Abs(shift); i++)
+			{
+				queue.Enqueue(queue.Dequeue());
+			}
+
+			return shift > 0 ? queue.Reverse().ToArray() : queue.ToArray();
+		}
+	}
+}
---
+++ CSharp/ArrayRotation/ArrayRotationTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CodeKatas.CSharp.ArrayRotation
+{
+	[TestFixture]
+	public class ArrayRotationTests
+	{
+		[TestCase(new int[0], -2, ExpectedResult = new int[0])]
+		[TestCase(new int[0], -1, ExpectedResult = new int[0])]
+		[TestCase(new int[0], 0, ExpectedResult = new int[0])]
+		[TestCase(new int[0], 1, ExpectedResult = new int[0])]
+		[TestCase(new int[0], 2, ExpectedResult = new int[0])]
+		[TestCase(new int[] { 1 }, -2, ExpectedResult = new int[] { 1 })]
+		[TestCase(new int[] { 1 }, -1, ExpectedResult = new int[] { 1 })]
+		[TestCase(new int[] { 1 }, 0, ExpectedResult = new int[] { 1 })]
+		[TestCase(new int[] { 1 }, 1, ExpectedResult = new int[] { 1 })]
+		[TestCase(new int[] { 1 }, 2, ExpectedResult = new int[] { 1 })]
+		[TestCase(new int[] { 1, 2, 3 }, -4, ExpectedResult = new int[] { 2, 3, 1 })]
+		[TestCase(new int[] { 1, 2, 3 }, -3, ExpectedResult = new int[] { 1, 2, 3 })]
+		[TestCase(new int[] { 1, 2, 3 }, -2, ExpectedResult = new int[] { 3, 1, 2 })]
+		[TestCase(new int[] { 1, 2, 3 }, -1, ExpectedResult = new int[] { 2, 3, 1 })]
+		[TestCase(new int[] { 1, 2, 3 }, 0, ExpectedResult = new int[] { 1, 2, 3 })]
+		[TestCase(new int[] { 1, 2, 3 }, 1, ExpectedResult = new int[] { 3, 1, 2 })]
+		[TestCase(new int[] { 1, 2, 3 }, 2, ExpectedResult = new int[] { 2, 3, 1 })]
+		[TestCase(new int[] { 1, 2, 3 }, 3, ExpectedResult = new int[] { 1, 2, 3 })]
+		[TestCase(new int[] { 1, 2, 3 }, 4, ExpectedResult = new int[] { 3, 1, 2 })]
+		public int[] RotateArrayWithFor(int[] arr, int shift)
+		{
+			return ArrayRotationEx.RotateArrayWithFor(arr, shift);
+		}
+
+		[TestCase(new int[0], -2, ExpectedResult = new int[0])]
+		[TestCase(new int[0], -1, ExpectedResult = new int[0])]
+		[TestCase(new int[0], 0, ExpectedResult = new int[0])]
+		[TestCase(new int[0], 1, ExpectedResult = new int[0])]
+		[TestCase(new int[0], 2, ExpectedResult = new int[0])]
+		[TestCase(new int[] { 1 }, -2, ExpectedResult = new int[] { 1 })]
+		[TestCase(new int[] { 1 }, -1, ExpectedResult = new int[] { 1 })]
+		[TestCase(new int[] { 1 }, 0, ExpectedResult = new int[] { 1 })]
+		[TestCase(new int[] { 1 }, 1, ExpectedResult = new int[] { 1 })]
+		[TestCase(new int[] { 1 }, 2, ExpectedResult = new int[] { 1 })]
+		[TestCase(new int[] { 1, 2, 3 }, -4, ExpectedResult = new int[] { 2, 3, 1 })]
+		[TestCase(new int[] { 1, 2, 3 }, -3, ExpectedResult = new int[] { 1, 2, 3 })]
+		[TestCase(new int[] { 1, 2, 3 }, -2, ExpectedResult = new int[] { 3, 1, 2 })]
+		[TestCase(new int[] { 1, 2, 3 }, -1, ExpectedResult = new int[] { 2, 3, 1 })]
+		[TestCase(new int[] { 1, 2, 3 }, 0, ExpectedResult = new int[] { 1, 2, 3 })]
+		[TestCase(new int[] { 1, 2, 3 }, 1, ExpectedResult = new int[] { 3, 1, 2 })]
+		[TestCase(new int[] { 1, 2, 3 }, 2, ExpectedResult = new int[] { 2, 3, 1 })]
+		[TestCase(new int[] { 1, 2, 3 }, 3, ExpectedResult = new int[] { 1, 2, 3 })]
+		[TestCase(new int[] { 1, 2, 3 }, 4, ExpectedResult = new int[] { 3, 1, 2 })]
+		public int[] RotateArrayWithQueue(int[] arr, int shift)
+		{
+			return ArrayRotationEx.RotateArrayWithQueue(arr, shift);
+		}
+	}
+}
